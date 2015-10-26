a90b429a8fc96901f45ef66fafccda376602aa42
carlesgm <carlesgm@gmail.com>
2015-10-26 20:58:15 +0100

    Creem un ArrayAdapter<String> buit i el connectem al ListView.

--- app/src/main/java/poblenou/rottentomatoesclient/MainActivityFragment.java
+++ app/src/main/java/poblenou/rottentomatoesclient/MainActivityFragment.java
@@ -5,11 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -19,7 +26,17 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        View lvPelis = rootView.findViewById(R.id.lvPelis);
+        ListView lvPelis = (ListView) rootView.findViewById(R.id.lvPelis);
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.lvpelis_row,
+                R.id.tvPeli,
+                items
+        );
+        lvPelis.setAdapter(adapter);
+
         return rootView;
     }
 }
