3a20df37d887ecc50d9456f806f9d313979c37e0
carlesgm <carlesgm@gmail.com>
2015-10-26 21:09:43 +0100

    Notifiquem a l'Activity que anem a agregar items de menu.

--- app/src/main/java/poblenou/rottentomatoesclient/MainActivityFragment.java
+++ app/src/main/java/poblenou/rottentomatoesclient/MainActivityFragment.java
@@ -23,6 +23,12 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
