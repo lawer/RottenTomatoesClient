d1a663e6760ee172cbfc1e4ce1d11a2d6362ee99
carlesgm <carlesgm@gmail.com>
2015-10-31 15:00:30 +0100

    Passem la pel·lícula clickada

--- app/src/main/java/poblenou/rottentomatoesclient/MainActivityFragment.java
+++ app/src/main/java/poblenou/rottentomatoesclient/MainActivityFragment.java
@@ -59,6 +59,7 @@ public class MainActivityFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent i = new Intent(getContext(), DetailActivity.class);
+                i.putExtra("movie", adapter.getItem(position));
                 startActivity(i);
             }
         });
