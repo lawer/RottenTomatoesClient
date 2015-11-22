25e794b19d9a16d00fcfdd82a88f2fe60ba8c308
lawer <carlesgm@gmail.com>
2015-11-22 23:26:44 +0100

    Added content provider

---
+++ app/src/main/java/poblenou/rottentomatoesclient/provider/movies/MoviesColumns.java
@@ -0,0 +1,65 @@
+package poblenou.rottentomatoesclient.provider.movies;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+import poblenou.rottentomatoesclient.provider.MoviesProvider;
+import poblenou.rottentomatoesclient.provider.movies.MoviesColumns;
+
+/**
+ * Columns for the {@code movies} table.
+ */
+public class MoviesColumns implements BaseColumns {
+    public static final String TABLE_NAME = "movies";
+    public static final Uri CONTENT_URI = Uri.parse(MoviesProvider.CONTENT_URI_BASE + "/" + TABLE_NAME);
+
+    /**
+     * Primary key.
+     */
+    public static final String _ID = BaseColumns._ID;
+
+    public static final String TITLE = "title";
+
+    public static final String CRITICSSCORE = "criticsScore";
+
+    public static final String AUDIENCESCORE = "audienceScore";
+
+    public static final String CONSENSUS = "consensus";
+
+    public static final String SYNOPSIS = "synopsis";
+
+    public static final String RELEASEDATE = "releaseDate";
+
+    public static final String SYNCTIME = "syncTime";
+
+
+    public static final String DEFAULT_ORDER = TABLE_NAME + "" +_ID;
+
+    // @formatter:off
+    public static final String[] ALL_COLUMNS = new String[] {
+            _ID,
+            TITLE,
+            CRITICSSCORE,
+            AUDIENCESCORE,
+            CONSENSUS,
+            SYNOPSIS,
+            RELEASEDATE,
+            SYNCTIME
+    };
+    // @formatter:on
+
+    public static boolean hasColumns(String[] projection) {
+        if (projection == null) return true;
+        for (String c : projection) {
+            if (c.equals(TITLE) || c.contains("" + TITLE)) return true;
+            if (c.equals(CRITICSSCORE) || c.contains("" + CRITICSSCORE)) return true;
+            if (c.equals(AUDIENCESCORE) || c.contains("" + AUDIENCESCORE)) return true;
+            if (c.equals(CONSENSUS) || c.contains("" + CONSENSUS)) return true;
+            if (c.equals(SYNOPSIS) || c.contains("" + SYNOPSIS)) return true;
+            if (c.equals(RELEASEDATE) || c.contains("" + RELEASEDATE)) return true;
+            if (c.equals(SYNCTIME) || c.contains("" + SYNCTIME)) return true;
+        }
+        return false;
+    }
+
+}
